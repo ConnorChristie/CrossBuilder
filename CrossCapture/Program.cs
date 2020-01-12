7a6519281227fdac5c5ef7c4d34dd92200d8121b
Connor Christie <chillercraft@hotmail.com>
2020-01-12 17:33:58 -0600

    Add readme

--- CrossCapture/Program.cs
+++ CrossCapture/Program.cs
@@ -12,8 +12,6 @@ namespace CrossCapture
     public class Program
     {
         private readonly INktSpyMgr spyMgr;
-        private INktHooksEnum hooks;
-
         private readonly IList<string> fileNames = new List<string>();
 
         public Program()
@@ -21,27 +19,25 @@ namespace CrossCapture
             spyMgr = new NktSpyMgr();
         }
 
-        public void Run(Action<IEnumerable<FileInfo>> onExit)
+        public void Run(string program, string arguments, string workingDir, Action<IEnumerable<FileInfo>> onExit)
         {
             spyMgr.Initialize();
-            hooks = spyMgr.CreateHooksCollection();
 
-            var Write_hook = spyMgr.CreateHook("Ntdll.dll!NtWriteFile", (int)(
+            var NtWriteFile_hook = spyMgr.CreateHook("Ntdll.dll!NtWriteFile", (int)(
                 eNktHookFlags.flgOnlyPostCall | eNktHookFlags.flgAutoHookChildProcess | eNktHookFlags.flgAutoHookActive));
 
-            Write_hook.OnFunctionCalled += OnNtWriteFile;
-            hooks.Add(Write_hook);
-            hooks.Hook(true);
+            NtWriteFile_hook.OnFunctionCalled += OnNtWriteFile;
+            NtWriteFile_hook.Hook(true);
 
-            var psi = new ProcessStartInfo(@"make.exe")
+            var psi = new ProcessStartInfo(program)
             {
-                Arguments = "install",
-                WorkingDirectory = @"D:\Git\librealsense\build"
+                Arguments = arguments,
+                WorkingDirectory = workingDir
             };
             var pc = Process.Start(psi);
             var proc = spyMgr.ProcessFromPID(pc.Id);
 
-            hooks.Attach(proc, true);
+            NtWriteFile_hook.Attach(proc, true);
 
             pc.EnableRaisingEvents = true;
             pc.Exited += (sender, e) =>
@@ -101,43 +97,7 @@ namespace CrossCapture
                 WinApi.CloseHandle(my_hFile);
             }
 
-            //var fileInfo = new FILE_NAME_INFO();
-            //var fileInfoSize = Marshal.SizeOf(fileInfo);
-            //var p_fileInfo = Marshal.AllocHGlobal(fileInfoSize);
-
-            //try
-            //{
-            //    Marshal.StructureToPtr(fileInfo, p_fileInfo, false);
-
-            //    if (WinApi.GetFileInformationByHandleEx(my_hFile, FileInformationClass.FileNameInfo, ref fileInfo, fileInfoSize))
-            //    {
-            //        var fileName2 = new StringBuilder(MAX_PATH);
-            //        var result = WinApi.GetFinalPathNameByHandle(my_hFile, fileName2, MAX_PATH, 0);
-
-            //        var fileName = new StringBuilder(fileInfo.FileNameLength);
-            //        var length = WinApi.GetFullPathName(fileInfo.FileName, fileInfo.FileNameLength, fileName, null);
-
-            //        return fileName2.ToString();
-            //    }
-            //}
-            //catch (Exception e)
-            //{
-            //    Console.WriteLine(e);
-            //}
-            //finally
-            //{
-            //    Marshal.FreeHGlobal(p_fileInfo);
-            //    WinApi.CloseHandle(my_hFile);
-            //}
-
             return null;
-        }
-
-        public static void Main(string[] args)
-        {
-            new Program().Run(x => { });
-
-            while (Console.ReadKey().Key != ConsoleKey.Escape);
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,48 @@
+# CrossBuilder
+This contains several utilities for cross compiling native projects on a Windows based host.
+
+## Highlights:
+* Dependency fetcher that emulates `apt-get`
+    * Downloads packages directly to the host computer instead of downloading them first to the target and copying them over.
+    * Prevents having to install dev packages on the target machine.
+* File capture that tracks written files when using commands such as `make install`
+    * Most cmake projects don't respect the install directory and instead write their files to all different directories.
+    * Allows you to copy captured files to your sysroot or staging directory.
+
+
+## Building OpenCV
+
+1. Install dependencies
+
+    ```sh
+    CrossBuilder.exe install -s <sysroot> libavcodec-dev libavformat-dev libswscale-dev libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libusb-1.0-0-dev libgtk-3-dev ffmpeg libgtk2.0-dev
+    ```
+
+2. Update ld scripts and remove absolute paths:
+
+    * `<sysroot>\lib\libgcc_s.so`
+    * `<sysroot>\usr\lib\arm-linux-gnueabihf\libc.so`
+    * `<sysroot>\usr\lib\arm-linux-gnueabihf\libpthread.so`
+    * `<sysroot>\usr\lib\gcc\arm-linux-gnueabihf\6\libgcc_s.so`
+    * `<sysroot>\usr\lib\libc.so`
+    * `<sysroot>\usr\lib\libpthread.so`
+
+    TODO: Need to have CrossBuilder do this automatically (search for "GNU ld script")
+
+3. Setup extra dependencies "dl m pthread rt" inside `/lib`
+
+    * Manually create `libm.so` inside `/lib`
+    * Manually create `libdl.so` inside `/lib`
+    * Manually create `librt.so` inside `/lib`
+    * Manually create `libpthread.so` inside `/lib`
+
+## Troubleshooting:
+
+### undefined reference to `__dlsym'
+    Probably don't have libdl.so or librt.so
+
+### ld.exe: cannot find /lib/arm-linux-gnueabihf/libc.so.6 inside
+    Probably didn't update and remove absolute paths in .so files
+
+### libm.a: error adding symbols: Bad value
+    Cannot find libm.so and resorts to using libm.a which is BAD. Create libm.so
